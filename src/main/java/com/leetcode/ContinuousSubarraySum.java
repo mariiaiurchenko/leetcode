4e0980b37934c0e665f60312f7cf7fd08c980140
Mariia Iurchenko <miurchenko@paypal.com>
2017-08-25 21:10:46 -0700

    Continuous Subarray Sum accepted

--- src/main/java/com/leetcode/ContinuousSubarraySum.java
+++ src/main/java/com/leetcode/ContinuousSubarraySum.java
@@ -2,11 +2,21 @@ package com.leetcode;
 
 public class ContinuousSubarraySum {
 
-    public static boolean checkSubarraySum(int[] nums, int k) {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        if (k == 0) {
+            return checkForZero(nums);
+        }
+
         int[] prefix = calcPrefixArray(nums, k);
-        for (int cur = 0; cur < nums.length; cur++) {
+        for (int cur = 1; cur < nums.length; cur++) {
+            if (prefix[cur] % k == 0 ) {
+                return true;
+            }
             for (int i = 0; i < cur - 1; i++) {
-                if ((prefix[cur] - prefix[i]) % k == 0 ) {
+                if ((prefix[cur] - prefix[i]) % k == 0) {
                     return true;
                 }
             }
@@ -14,19 +24,30 @@ public class ContinuousSubarraySum {
         return false;
     }
 
-    private static int[] calcPrefixArray(int[] nums, int k) {
+    private int[] calcPrefixArray(int[] nums, int k) {
         int[] prefix = new int[nums.length];
         prefix[0] = nums[0];
-        for (int i = 1; i < nums.length;i++) {
-            prefix[i] = (prefix[i - 1] + nums[i]) %k;
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = (prefix[i - 1] + nums[i]) % k;
         }
         return prefix;
     }
 
+    private boolean checkForZero(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == 0 && nums[i] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String args[]) {
-        System.out.println(checkSubarraySum(new int[] {23, 2, 4, 6, 7}, 6));
-        System.out.println(checkSubarraySum(new int[] {23, 2, 6, 4, 7}, 6));
-        System.out.println(checkSubarraySum(new int[] {1, 1, 1, 1, 1}, 6));
+        ContinuousSubarraySum solution = new ContinuousSubarraySum();
+        System.out.println(solution.checkSubarraySum(new int[] {0, 0}, -1) + " -> true");
+        System.out.println(solution.checkSubarraySum(new int[] {23, 2, 4, 6, 7}, 6) + " -> true" );
+        System.out.println(solution.checkSubarraySum(new int[] {23, 2, 6, 4, 7}, 6) + " -> true");
+        System.out.println(solution.checkSubarraySum(new int[] {1, 1, 1, 1, 1}, 6) + " -> false");
     }
 
 }
--- src/main/java/com/leetcode/PermutationsII.java
+++ src/main/java/com/leetcode/PermutationsII.java
@@ -3,6 +3,17 @@ package com.leetcode;
 import java.util.LinkedList;
 import java.util.List;
 
+/*
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+* */
 public class PermutationsII {
 
     public List<List<Integer>> permuteUnique(int[] nums) {
