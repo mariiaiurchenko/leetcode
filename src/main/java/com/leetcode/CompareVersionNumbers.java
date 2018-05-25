f5a0d30b597024d785c191b9600c3f4299703a14
Mariia Iurchenko <miurchenko@paypal.com>
2018-05-25 16:08:04 -0700

    message

--- src/main/java/com/leetcode/CompareVersionNumbers.java
+++ src/main/java/com/leetcode/CompareVersionNumbers.java
@@ -23,7 +23,78 @@ package com.leetcode;
  */
 public class CompareVersionNumbers {
 
+    private static final int VERSION_1 = 1;
+    private static final int VERSION_2 = -1;
+    private static final int NOT_COMPARABLE = 0;
+
+    public static void main(String[] args) {
+        CompareVersionNumbers compareVersionNumbers = new CompareVersionNumbers();
+        System.out.println(compareVersionNumbers.compareVersion("1.1", "1.10") + "   = -1");
+        System.out.println(compareVersionNumbers.compareVersion("1", "1.0") + "   = 0");
+        System.out.println(compareVersionNumbers.compareVersion("1.2", "1.10") + "   = -1");
+        System.out.println(compareVersionNumbers.compareVersion("1", "0") + "   = 1");
+        System.out.println(compareVersionNumbers.compareVersion("0.1", "1.1") + "   = -1");
+        System.out.println(compareVersionNumbers.compareVersion("1.0.1", "1") + "   = 1");
+        System.out.println(compareVersionNumbers.compareVersion("7.5.2.4", "7.5.3") + "   = -1" );
+        System.out.println(compareVersionNumbers.compareVersion("1", "01") + "   = 0");
+    }
+
     public int compareVersion(String version1, String version2) {
-        return 0;
+
+        int i1 = 0;
+        int i2 = 0;
+        i1 = skipZero(version1, i1);
+        i2 = skipZero(version2, i2);
+        while (i1 < version1.length() && i2 < version2.length() ) {
+            if (version1.charAt(i1) == version2.charAt(i2)) {
+                if (version1.charAt(i1) == '.') {
+                    i1++;
+                    i2++;
+                    i1 = skipZero(version1, i1);
+                    i2 = skipZero(version2, i2);
+                } else {
+                    i1++;
+                    i2++;
+                }
+            } else {
+                int bigger = version1.charAt(i1) > version2.charAt(i2) ? VERSION_1 : VERSION_2;
+                while (i1 < version1.length() && i2 < version2.length()
+                    && version1.charAt(i1) != '.' && version2.charAt(i2) != '.') {
+                  i1++;
+                  i2++;
+                }
+                if ((i1 == version1.length() || version1.charAt(i1) == '.')
+                    && (i2 == version2.length() || version2.charAt(i2) == '.')) {
+                    return bigger;
+                } else {
+                }
+                return i1 == version1.length() || version1.charAt(i1) == '.' ? VERSION_2 : VERSION_1;
+            }
+
+        }
+        i1 = skipZeroAndDot(version1, i1);
+        i2 = skipZeroAndDot(version2, i2);
+
+        if (i1 < version1.length() || i2 < version2.length()) {
+            return i1 < version1.length() ? VERSION_1 : VERSION_2;
+        }
+
+        return NOT_COMPARABLE;
+    }
+
+    private int skipZeroAndDot(String version, int i) {
+        if (i<version.length() && version.charAt(i) == '.') {
+            while (i < version.length() && (version.charAt(i) == '0' || version.charAt(i) == '.')) {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    private int skipZero(String version1, int i1) {
+        while (i1 < version1.length() && version1.charAt(i1) == '0') {
+            i1++;
+        }
+        return i1;
     }
 }
